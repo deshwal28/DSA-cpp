dfa5be4ab01c611acb0d48ae07ce6e0bcba8a7bb
deshwal28 <deshwalshab65@gmail.com>
2024-04-17 01:41:36 +0530

    first commit

---
+++ Basis/1_In/2_variable.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //variable declare 4 bytes
+   // int a=10,b=20,c=30,d=1;
+   // cout<<a+c-d*b/a;
+
+    //char c='a';//1 byte
+    //cout<<c;
+
+  //float f = 2.55;//4 byte
+   //cout<<f;
+
+   //double d = 3.12145678;
+   //cout<<d;
+
+   bool b = true;//1  byte
+   cout<<b;
+
+
+
+}
