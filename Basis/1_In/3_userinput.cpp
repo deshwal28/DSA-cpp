9d02076352a00b2185a7f73d3e5c2258393b598f
deshwal28 <deshwalshab65@gmail.com>
2024-04-18 13:30:00 +0530

    cpp

---
+++ Basis/1_In/3_userinput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    //int a,b;
+    //cout<<"Enter your first Number";
+    //cin>>a;
+    //cout<<"Enter your second Number";
+    //cin>>b;
+    //cout<<"Sum of Number is:"<<a+b;
+    int a=10;
+    char c= 'c';
+    a=c;
+    cout<<a;
+
+ }
