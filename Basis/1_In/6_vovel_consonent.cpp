fd119454b272e311046de26810c34a4f6bed143e
deshwal28 <deshwalshab65@gmail.com>
2024-04-18 18:23:27 +0530

    comit

---
+++ Basis/1_In/6_vovel_consonent.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    char c;
+    cout<<"Enter a vovel:";
+    cin>>c;
+    if(c=='a')
+    cout<<"Vovel";
+    else if(c=='e')
+     cout<<"Vovel";
+    else if(c=='i')
+     cout<<"Vovel";
+    else if(c=='o')
+     cout<<"Vovel";
+    else if(c=='u') 
+    cout<<"Vovel";
+    else{
+        cout<<"consonant";
+    }
+
+ }
