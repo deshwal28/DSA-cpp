81d3d7a1a10af6c796ff538311e7ee28580447c2
deshwal28 <deshwalshab65@gmail.com>
2024-04-18 13:30:37 +0530

    comit

---
+++ Basis/1_In/4_ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main (){
+    int package;
+    cin>>package;
+    if(package>10)
+    {
+        cout<<"Acepted";
+    }
+    else
+    {
+        cout<<"rejected";
+    }
+
+ }
