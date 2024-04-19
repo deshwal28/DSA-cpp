04d94579ca89f57f3d8bf8660922f3e0f40db1bf
deshwal28 <deshwalshab65@gmail.com>
2024-04-19 18:24:44 +0530

    comit

---
+++ 12_table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"Enter a number:";
+    cin>>n;
+    for( i=1;i<=10;i=i+1)
+    {
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    }
+
+}
