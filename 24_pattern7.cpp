3d644d8d0b2b63303c92734d817000113d31f84e
deshwal28 <deshwalshab65@gmail.com>
2024-04-20 11:56:32 +0530

    coit

---
+++ 24_pattern7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row;
+    char col;
+    for(row=1;row<=5;row=row+1)
+    {
+        
+        for(char col='a';col<='e';col=col+1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl; 
+        
+    }
+}
