0e69aadc5b3d7dc4cefbeb41fa2ad24a9df136c9
deshwal28 <deshwalshab65@gmail.com>
2024-05-05 12:45:20 +0530

    comit

---
+++ 39_star6.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row ,col,n;
+    cout<<"Enter the input:";
+    cin>>n;
+    for(row=n;row>=1;row=row-1)
+    {
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        for(col=1;col<=2*n-2*row;col=col+1)
+        cout<<"  ";
+
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        cout<<endl;
+        
+    }
+    for(row=1;row<=n;row=row+1)
+    {
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        for(col=1;col<=2*n-2*row;col=col+1)
+        cout<<"  ";
+
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        cout<<endl;
+    }
+    
+}
