056a2e6b7a4615dc49cc3069506291201da9ba90
deshwal28 <deshwalshab65@gmail.com>
2024-05-05 12:48:42 +0530

    cu

---
+++ 48_switch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter a number:";
+    cin>>i;
+    switch (i)
+    {
+    case 1:
+    cout<<"Khus";
+    break;
+
+    case 2:
+    cout<<"Deshwal";
+    break;
+    
+    default:
+    cout<<"Aadi";
+     
+    
+    }
+
+
+}
