6642cabadd76759281baf57d79e2f7eb14c6c134
deshwal28 <deshwalshab65@gmail.com>
2024-05-05 12:45:52 +0530

    comi

---
+++ 40_star7.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col,n;
+    cout<<"Enter a number:";
+    cin>>n;
+    for(row=1;row<=n;row=row+1)
+    {
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        for(col=1;col<=2*n-2*row;col=col+1)
+        cout<<"  ";
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        cout<<endl;
+    }
+    for(row=n-1;row>=1;row=row-1)
+    {
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        for(col=1;col<=2*n-2*row;col=col+1)
+        cout<<"  ";
+        for(col=1;col<=row;col=col+1)
+        cout<<"* ";
+        cout<<endl;
+    }
+}
