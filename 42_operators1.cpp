c82bbf8aa0d9aed6671c5ffef0a8c17acf447f56
deshwal28 <deshwalshab65@gmail.com>
2024-05-05 12:47:03 +0530

    comit

---
+++ 42_operators1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a:";
+    cin>>a;
+    cout<<"Enter a value of b :";
+    cin>>b;
+
+    //if (a>b)
+   // if(a==b)
+   //if (a<b)
+  // if (a<=b)
+  if (a!=b)
+    cout<<"Yes";
+    else
+    cout<<"No";
+}
