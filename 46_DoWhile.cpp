27000e16150f95ef5a4c7dd78f2a478bf18da837
deshwal28 <deshwalshab65@gmail.com>
2024-05-05 12:48:12 +0530

    ch

---
+++ 46_DoWhile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+     int i= 1; //initialize
+     do{
+        cout<<i<<endl;
+        i++;//update
+     }while(i<=n);//break condition
+}
