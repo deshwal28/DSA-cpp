6de2d2ae5889f69a982c8f9064363c4a6384c3d0
deshwal28 <deshwalshab65@gmail.com>
2024-08-14 23:51:23 +0530

    commit

---
+++ Basis/1_In/binarysearch.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int BinarySearch(int arr[],int n, int key)
+{
+  //start,end,mid;
+  int start=0,end=n-1,mid;
+  while(start<=end)
+  {
+    mid=(start+end)/2;
+    if(arr[mid]==key)
+    return mid;
+    else if(arr[mid]<key)
+    start=mid+1;
+    else
+    end=mid-1;
+
+  }
+  return-1;
+}
+int main()
+{
+  int arr[1000];
+  int n;
+  cout<<"Enter the number of element in Array";
+  cin>>n;
+  cout<<"Enter the elements:";
+  for(int i=0;i<n;i++)
+  cin>>arr[i];
+
+  int key;
+  cout<<"Enter the key:";
+  cin>>key;
+  cout<<BinarySearch(arr,n,key)<<endl;
+  return 0 ;
+
+} 
