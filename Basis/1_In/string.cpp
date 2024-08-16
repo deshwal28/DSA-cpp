afc6ced2e7e701b9e62a9426d01f9afa0aedf452
deshwal28 <deshwalshab65@gmail.com>
2024-08-16 13:01:50 +0530

    commit

---
+++ Basis/1_In/string.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std ; 
+ int main()
+ {
+    //char arr[]={'a','p','p','l','e'};
+   // for(int i = 0 ; i<5;i++)
+    //cout<<arr[i];
+    //char arr[20];
+    //cin>>arr;
+    //arr[20]='\0';
+    //cout<<arr;
+    /*string s;
+    getline(cin,s);
+    cout<<s<<endl;
+    cout<<s.size();
+    string s1="Adi",s2="chaudhary";
+    s1=s1+'z';
+    cout<<1;*/
+
+    //escape character
+    string s= "Adi caudhary is a \"good\" boy";
+    cout<<s;                                         
+ }
+    
+
+    
+ 
